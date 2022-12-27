f26cc1c420c520608d8ec7b2c18bf01cc81a415e
pit-lzc <1196445102@qq.com>
2022-12-27 22:52:03 +0800

    初始结构

---
+++ gulimall-member/src/test/java/com/lzc/gulimall/product/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzc.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
